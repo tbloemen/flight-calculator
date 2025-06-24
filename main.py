3bdf02cc7008684a74844d8bd778d7260c1470d2
Ties Bloemen <t.t.j.bloemen@student.tudelft.nl>
2025-06-24 18:32:00 +0200

    wip on output code

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-from src.analysis import get_average_cost
+from src.analysis import Advice, get_average_cost
 from src.flights import (
     FlightRequest,
     get_parsed_flights,
 )
 from src.input import take_sheet_input
+from src.output import create_pdf
 
 
-def print_result(price: int, minutes: int):
-    print(
-        f"The average sufficient price is {price} euros. The average duration is {int(minutes/60)} hours and {minutes % 60 } minutes."
-    )
-
-
-def analyze_request(requests: list[tuple[str, FlightRequest | None]]) -> None:
+def analyze_request(requests: list[tuple[str, FlightRequest | None]]) -> list[Advice]:
+    advices = []
     for i, (name, request) in enumerate(requests):
         if request is None:
             continue
-        filename = f"{i}_{name}.png"
+        filename = f"{i}_{name.replace(' ', '_')}.png"
         flights_now = get_parsed_flights(request)
-        advice = get_average_cost(flights_now, filename)
-        print(advice)
+        advice = get_average_cost(flights_now, filename, name, request.host_currency)
+        advices.append(advice)
+    return advices
 
 
 def main():
@@ -28,7 +25,8 @@ def main():
     # analyze_request(request)
     requests = take_sheet_input("input.ods")
     print(requests)
-    analyze_request(requests)
+    advices = analyze_request(requests)
+    create_pdf(advices)
 
 
 if __name__ == "__main__":
--- src/output.py
+++ src/output.py
@@ -1,5 +1,45 @@
-from analysis import Advice
+import pendulum
+import subprocess
+from jinja2 import Environment, FileSystemLoader
+
+from .analysis import Advice
 
 
-def create_pdf(advises: Advice) -> None:
-    pass
+def convert_md_to_pdf(input_file: str, output_file: str = "output.pdf") -> str:
+    try:
+        subprocess.run(
+            [
+                "pandoc",
+                input_file,
+                "-o",
+                output_file,
+                "--pdf-engine=tectonic",
+                "-V",
+                "geometry:margin=2cm",
+            ]
+        )
+    except subprocess.CalledProcessError as e:
+        print("Error during pdf creation:", e)
+    finally:
+        return output_file
+
+
+def convert_advices_to_md(advices: list[Advice]) -> str:
+    env = Environment(loader=FileSystemLoader("src/templates/"))
+    template = env.get_template("report.md")
+    filename = "test.md"
+
+    content = template.render(
+        advices=advices,
+        date=pendulum.now().to_formatted_date_string(),
+    )
+
+    with open(filename, mode="w", encoding="utf-8") as output:
+        output.write(content)
+    return filename
+
+
+def create_pdf(advices: list[Advice]) -> None:
+    md_file = convert_advices_to_md(advices)
+    pdf_file = convert_md_to_pdf(md_file)
+    print("The report has been generated:", pdf_file)
